9d5b96e650478026917cf1b6e8a5c50230cab98f
Ahblisk <jnotoma1@students.kennesaw.edu>
2020-06-01 23:50:58 -0400

    Added JS page, added style sheet, NOTE: Fix button

---
+++ js/hamburger.js
@@ -0,0 +1,8 @@
+function burger() {
+    var x = document.getElementById("displayLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  } 
